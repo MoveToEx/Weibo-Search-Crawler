f756d2287946d9f32457b5a0c4d2f1d7c4bb60ba
MoveToEx <movetoex@outlook.com>
2023-05-24 20:06:21 +0800

    Update

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 from selenium import webdriver
 from urllib import parse
 import bs4
-
+import json
 import html.parser
+import re
 
 browser = webdriver.Chrome()
-
 browser.minimize_window()
+browser.get('https://weibo.com')
+
+cookies = []
+
+def parse_int(s: str) -> int:
+    s = s.strip()
+    if m := re.match(r'(\d+)万\+?', s):
+        return int(m.group(1)) * 10000
+    elif s.isdigit():
+        return int(s)
+    else:
+        return 0
 
 try:
-    like_sum = 0
-    comment_sum = 0
-    share_sum = 0
-    post_sum = 0
+    with open('cookies.json', 'r', encoding='utf8') as f:
+        cookies = json.loads(f.read())
+        for i in cookies:
+            browser.add_cookie(i)
+except FileNotFoundError:
+    open('cookies.json', 'x')
 
-    text = parse.quote(input("Query text: "))
+like_sum = 0
+comment_sum = 0
+share_sum = 0
+post_sum = 0
 
-    browser.get(f'https://s.weibo.com/weibo?q={text}&nodup=1&page=1')
+text = parse.quote(input("Query text: "))
 
+browser.get(f'https://s.weibo.com/weibo?q={text}&nodup=1&page=1')
+
+if not cookies:
     browser.get(f'https://weibo.com/login.php')
     input("请在弹出窗口中登录您的账号，完成后请按回车")
 
-    cookies = browser.get_cookies()
-
-    for i in cookies:
-        browser.add_cookie({
+    cookies = [
+        {
             'domain': "weibo.com",
             'name': i.get('name'),
             'value': i.get('value'),
@@ -33,42 +51,55 @@ try:
             'httpOnly': False,
             'HostOnly': False,
             'Secure': False
-        })
+        } for i in browser.get_cookies()
+    ]
 
-    browser.refresh()
+    for i in cookies:
+        browser.add_cookie(i)
 
-    print('[INFO] Successfully imported cookies')
+    with open('cookies.json', 'w', encoding='utf8') as f:
+        f.write(json.dumps(cookies, ensure_ascii=False))
 
-    max_page = 50
-    print("开始爬取......")
-    for page in range(1, max_page+1):
-        browser.get(f'https://s.weibo.com/weibo?q={text}&nodup=1&page={page}')
+browser.refresh()
 
-        if browser.page_source.find('抱歉，未找到相关结果。') != -1:
-            continue
+print('[INFO] Successfully imported cookies')
 
-        html = bs4.BeautifulSoup(browser.page_source, 'html.parser')
+max_page = 50
 
-        items = [x.select_one('.card-act') for x in html.select(
-            'div[action-type="feed_list_item"]') if not x.select('.card-comment')]
+print("[INFO] Starting crawler...")
+for page in range(1, max_page+1):
+    browser.get(f'https://s.weibo.com/weibo?q={text}&nodup=1&page={page}')
+
+    if browser.page_source.find('抱歉，未找到相关结果。') != -1:
+        continue
+
+    html = bs4.BeautifulSoup(browser.page_source, 'html.parser')
+
+    items = [x.select_one('.card-act') for x in html.select(
+        'div[action-type="feed_list_item"]') if not x.select('.card-comment')]
+    
+    post_sum += len(items)
+    
+    for i in items:
+        likes = i.select_one('.woo-like-count').text.strip()
+        comments = i.select_one('a[action-type="feed_list_comment"]').text.strip()
+        forwards = i.select_one('a[action-type="feed_list_forward"]').text.strip()
         
-        post_sum += len(items)
-        
-        for i in items:
-            likes = i.select_one('.woo-like-count').text.strip()
-            comments = i.select_one('a[action-type="feed_list_comment"]').text.strip()
-            forwards = i.select_one('a[action-type="feed_list_forward"]').text.strip()
-            
-            likes = int(likes) if likes.isdigit() else 0
-            comments = int(comments) if comments.isdigit() else 0
-            forwards = int(forwards) if forwards.isdigit() else 0
+        likes = parse_int(likes)
+        comments = parse_int(comments)
+        forwards = parse_int(forwards)
 
-            like_sum += likes
-            comment_sum += comments
-            share_sum += forwards
+        like_sum += likes
+        comment_sum += comments
+        share_sum += forwards
 
-        print(f"[INFO] Page {page} finished: post={post_sum}, like={like_sum}, comment={comment_sum}, share={share_sum}")
+    print(f"[INFO] Page {page} finished: post={post_sum}, like={like_sum}, comment={comment_sum}, share={share_sum}")
 
-    print(f"转发数：{share_sum} 评论数：{comment_sum} 点赞数：{like_sum} 帖子数：{post_sum}")
-finally:
-    browser.close()
+browser.close()
+print("[INFO] Crawler stopped")
+
+print(f"帖子数：{post_sum}")
+print(f"点赞数：{like_sum}")
+print(f"评论数：{comment_sum}")
+print(f"转发数：{share_sum}")
+
